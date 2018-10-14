9e07ae04811562e20728eb86a92595644a790970
iamn080dy <39668148+iamn080dy@users.noreply.github.com>
2018-10-15 00:45:16 +0800

    Add files via upload

---
+++ XML_Analysis/XML_Analysis/Models/OpenData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenDataImport.Models
+{
+	public class OpenData
+	{
+        public String 測站名稱 { get; set; }  // SiteName
+        public String 縣市 { get; set; }      // County
+        public String 監測日期 { get; set; }  // MonitorDate
+        public String 數值 { get; set; }      // Concentration
+    }
+}
---
+++ XML_Analysis/XML_Analysis/Program.cs
@@ -0,0 +1,60 @@
+﻿using OpenDataImport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XML_Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodes = findOpenData();
+            showOpenData(nodes);
+            Console.ReadKey();
+        }
+
+        static List<OpenData> findOpenData()
+        {
+            List<OpenData> result = new List<OpenData>();
+
+            var xml = XElement.Load(@"../../../Data/opendata.epa.gov.tw.xml");
+
+            var nodes = xml.Descendants("Data").ToList();
+
+            for (var i = 0; i < nodes.Count; i++)
+            {
+                var node = nodes[i];
+                OpenData item = new OpenData();
+
+                item.縣市 = getValue(node, "County");
+                item.測站名稱 = getValue(node, "SiteName");
+                item.監測日期 = getValue(node, "MonitorDate");
+                item.數值 = getValue(node, "Concentration");
+                result.Add(item);
+            }
+
+            return result;
+        }
+        
+        private static String getValue(XElement node, String propertyName)
+        {
+            return node.Element(propertyName)?.Value?.Trim();
+        }
+
+        private static void showOpenData(List<OpenData> nodes)
+        {
+            Console.WriteLine(String.Format("共收到{0}筆的資料", nodes.Count));
+            nodes.GroupBy(node => node.縣市).ToList().ForEach(group =>
+            {
+                var key = group.Key;
+                var groupDatas = group.ToList();
+                var message = $"{key}, 共有{groupDatas.Count()}筆資料";
+                Console.WriteLine(message);
+            });
+
+        }
+
+    }
+}
